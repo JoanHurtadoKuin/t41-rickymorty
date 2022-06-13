28be1409c1872172d5ea5a9e04ebee88a4fa1f9f
diablillo99lp <joan.hurtado.garcia@gmail.com>
2022-06-13 21:51:26 +0200

    T41

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 't41-rickymorty'`, () => {
+  it(`should have as title 't37-rickymorty'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('t41-rickymorty');
+    expect(app.title).toEqual('t37-rickymorty');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('t41-rickymorty app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('t37-rickymorty app is running!');
   });
 });
